28d7ee7504306859b19e36f0d833e8594f08e03c
eparly <ethanmparliament@gmail.com>
2023-03-03 11:56:17 -0500

    commented out ram

--- vhdl_files/RAM_inst.vhd
+++ vhdl_files/RAM_inst.vhd
@@ -1,8 +1,8 @@
-ram_inst : ram PORT MAP (
-		address	 => address_sig,
-		clock	 => clock_sig,
-		data	 => data_sig,
-		rden	 => rden_sig,
-		wren	 => wren_sig,
-		q	 => q_sig
-	);
+--ram_inst : ram PORT MAP (
+--		address	 => address_sig,
+--		clock	 => clock_sig,
+--		data	 => data_sig,
+--		rden	 => rden_sig,
+--		wren	 => wren_sig,
+--		q	 => q_sig
+--	);
